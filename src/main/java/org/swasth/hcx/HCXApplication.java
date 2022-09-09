8fe51403e885e677608c719066a2af934d751f74
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2022-09-09 15:19:07 +0530

    updated app.yaml for staging env as devops is not able to replace

--- src/main/java/org/swasth/hcx/HCXApplication.java
+++ src/main/java/org/swasth/hcx/HCXApplication.java
@@ -1,12 +1,15 @@
 package org.swasth.hcx;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class HCXApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HCXApplication.class, args);
 	}
