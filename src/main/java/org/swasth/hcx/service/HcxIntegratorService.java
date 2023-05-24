335b90b280ba824378dc23bfa4db4114808b2ad3
shiva-rakshith <rakshiths@sanketika.in>
2023-05-24 19:04:16 +0530

    feat: update integrator sdk version

--- src/main/java/org/swasth/hcx/service/HcxIntegratorService.java
+++ src/main/java/org/swasth/hcx/service/HcxIntegratorService.java
@@ -65,6 +65,7 @@ public class HcxIntegratorService {
         configMap.put("username", username);
         configMap.put("password", password);
         configMap.put("encryptionPrivateKey", privateKey);
+        configMap.put("fhirValidationEnabled", false);
 
         return configMap;
     }
