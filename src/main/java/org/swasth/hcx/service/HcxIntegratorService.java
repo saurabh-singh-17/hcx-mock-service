fbd4c97f531393ac334c646936cd262e18ec0f85
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-07-24 11:50:36 +0530

    feat : added signingPrivateKey

--- src/main/java/org/swasth/hcx/service/HcxIntegratorService.java
+++ src/main/java/org/swasth/hcx/service/HcxIntegratorService.java
@@ -67,6 +67,7 @@ public class HcxIntegratorService {
         configMap.put("password", password);
         configMap.put("encryptionPrivateKey", privateKey);
         configMap.put("fhirValidationEnabled", false);
+        configMap.put("signingPrivateKey", privateKey);
 
         return configMap;
     }
