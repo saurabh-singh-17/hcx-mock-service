429643f892658b132af8bd0fbc1105bd003e3a48
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-11-27 10:40:33 +0530

    feat : document type changes

--- src/main/java/org/swasth/hcx/service/PayerService.java
+++ src/main/java/org/swasth/hcx/service/PayerService.java
@@ -33,7 +33,7 @@ public class PayerService {
 
     public void process(Request request, String reqFhirObj, String respFhirObj) throws ClientException, JsonProcessingException {
         Map<String,Object> info = new HashMap<>();
-        List<String> getDocuments = new ArrayList<>();
+        Map<String,List<String>> getDocuments = new HashMap<>();
         String amount =  "";
         if(!request.getAction().contains("coverageeligibility")) {
             info.put("medical", Collections.singletonMap("status", PENDING));
@@ -43,7 +43,6 @@ public class PayerService {
         }
         String query = String.format("INSERT INTO %s (request_id,sender_code,recipient_code,action,raw_payload,request_fhir,response_fhir,status,additional_info,created_on,updated_on,correlation_id,mobile,otp_verification,workflow_id,account_number,ifsc_code,bank_details,app,supporting_documents,bill_amount,insurance_id,patient_name) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');",
                 table, request.getApiCallId(), request.getSenderCode(), request.getRecipientCode(), getEntity(request.getAction()), request.getPayload().getOrDefault("payload", ""), reqFhirObj, respFhirObj, PENDING, JSONUtils.serialize(info), System.currentTimeMillis(), System.currentTimeMillis(), request.getCorrelationId(), "", PENDING, request.getWorkflowId(), "1234", "1234", PENDING, "", getDocuments, amount, getInsuranceId(reqFhirObj), getPatientName(reqFhirObj));
-        System.out.println("-------------query -----------" + query);
         postgres.execute(query);
     }
 
@@ -55,34 +54,21 @@ public class PayerService {
         actionMap.put("/communication/request", "communication");
         return actionMap.get(action);
     }
-//    public Map<String, List<String>> getSupportingDocuments(String fhirPayload) {
-//        Bundle parsed = parser.parseResource(Bundle.class, fhirPayload);
-//        Claim claim = parser.parseResource(Claim.class, parser.encodeResourceToString(parsed.getEntry().get(0).getResource()));
-//        Map<String, List<String>> documentMap = new HashMap<>();
-//        for (Claim.SupportingInformationComponent supportingInfo : claim.getSupportingInfo()) {
-//            if (supportingInfo.hasValueAttachment() && supportingInfo.getValueAttachment().hasUrl()) {
-//                String url = supportingInfo.getValueAttachment().getUrl();
-//                String documentType = supportingInfo.getCategory().getCoding().get(0).getDisplay();
-//                if (!documentMap.containsKey(documentType)) {
-//                    documentMap.put(documentType, new ArrayList<>());
-//                }
-//                documentMap.get(documentType).add(url);
-//            }
-//        }
-//        return documentMap;
-//    }
-
-    public List<String> getSupportingDocuments(String fhirPayload) {
+    public Map<String, List<String>> getSupportingDocuments(String fhirPayload) {
         Bundle parsed = parser.parseResource(Bundle.class, fhirPayload);
         Claim claim = parser.parseResource(Claim.class, parser.encodeResourceToString(parsed.getEntry().get(0).getResource()));
-        List<String> documentUrls = new ArrayList<>();
+        Map<String, List<String>> documentMap = new HashMap<>();
         for (Claim.SupportingInformationComponent supportingInfo : claim.getSupportingInfo()) {
             if (supportingInfo.hasValueAttachment() && supportingInfo.getValueAttachment().hasUrl()) {
                 String url = supportingInfo.getValueAttachment().getUrl();
-                documentUrls.add(url);
+                String documentType = supportingInfo.getCategory().getCoding().get(0).getDisplay();
+                if (!documentMap.containsKey(documentType)) {
+                    documentMap.put(documentType, new ArrayList<>());
+                }
+                documentMap.get(documentType).add(url);
             }
         }
-        return documentUrls;
+        return documentMap;
     }
 
     public String getAmount(String fhirPayload) {
