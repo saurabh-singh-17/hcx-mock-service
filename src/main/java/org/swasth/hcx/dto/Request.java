13ef3a568efdc94f85fd2ae2d582faadec7bf5fa
Abhishek Gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2024-01-17 19:23:26 +0530

    Update Request.java

--- src/main/java/org/swasth/hcx/dto/Request.java
+++ src/main/java/org/swasth/hcx/dto/Request.java
@@ -22,6 +22,7 @@ public class Request {
         this.action = action;
         if (NOTIFICATION_NOTIFY.equals(action)) {
             String encodedPayload = (String) ((Map<String, Object>) requestBody.get(PAYLOAD)).get(PAYLOAD);
+            System.out.println("Encoded payload ----------" + JSONUtils.serialize(encodedPayload));
             hcxHeaders = JSONUtils.decodeBase64String(encodedPayload.split("\\.")[0], Map.class);
             payload = JSONUtils.decodeBase64String(encodedPayload.split("\\.")[1], Map.class);
         } else {
