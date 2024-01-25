3aeebeed8121244052215a9765513d2b271f3da7
Abhishek Gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2024-01-25 17:46:42 +0530

    Fix : fixed communication request

--- src/main/java/org/swasth/hcx/service/PayerService.java
+++ src/main/java/org/swasth/hcx/service/PayerService.java
@@ -34,8 +34,13 @@ public class PayerService {
     public void process(Request request, String reqFhirObj, String respFhirObj) throws ClientException, JsonProcessingException {
         Map<String, Object> info = new HashMap<>();
         String app = getApp(request.getAction(), reqFhirObj);
+        if (request.getAction().contains(Constants.COMMUNICATION)) {
+            String query = String.format("INSERT INTO %s (request_id,sender_code,recipient_code,action,raw_payload,request_fhir,response_fhir,status,additional_info,created_on,updated_on,correlation_id,mobile,otp_verification,workflow_id,account_number,ifsc_code,bank_details,app,supporting_documents,bill_amount,insurance_id,patient_name) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');",
+                    table, request.getApiCallId(), request.getSenderCode(), request.getRecipientCode(), getEntity(request.getAction()), request.getPayload().getOrDefault(Constants.PAYLOAD, ""), reqFhirObj, respFhirObj, PENDING, JSONUtils.serialize(info), System.currentTimeMillis(), System.currentTimeMillis(), request.getCorrelationId(), "", PENDING, request.getWorkflowId(), "1234", "1234", PENDING, "", "{}", "", "", "");
+            postgres.execute(query);
+        }
         if (!StringUtils.isEmpty(app)) {
-            processWithApp(request, info, reqFhirObj, respFhirObj,app);
+            processWithApp(request, info, reqFhirObj, respFhirObj, app);
         } else {
             processWithOutApp(request, info, reqFhirObj, respFhirObj);
         }
@@ -57,9 +62,6 @@ public class PayerService {
         if (action.contains(Constants.COVERAGE_ELIGIBILITY)) {
             query = String.format("INSERT INTO %s (request_id,sender_code,recipient_code,action,raw_payload,request_fhir,response_fhir,status,additional_info,created_on,updated_on,correlation_id,mobile,otp_verification,workflow_id,account_number,ifsc_code,bank_details,app,supporting_documents,bill_amount,insurance_id,patient_name) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');",
                     table, request.getApiCallId(), request.getSenderCode(), request.getRecipientCode(), getEntity(request.getAction()), request.getPayload().getOrDefault(Constants.PAYLOAD, ""), reqFhirObj, respFhirObj, PENDING, JSONUtils.serialize(info), System.currentTimeMillis(), System.currentTimeMillis(), request.getCorrelationId(), getPatientMobile(reqFhirObj), PENDING, request.getWorkflowId(), "1234", "1234", PENDING, app, "{}", "", getInsuranceId(reqFhirObj), getPatientName(reqFhirObj));
-        } else if (action.contains(Constants.COMMUNICATION)) {
-            query = String.format("INSERT INTO %s (request_id,sender_code,recipient_code,action,raw_payload,request_fhir,response_fhir,status,additional_info,created_on,updated_on,correlation_id,mobile,otp_verification,workflow_id,account_number,ifsc_code,bank_details,app,supporting_documents,bill_amount,insurance_id,patient_name) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');",
-                    table, request.getApiCallId(), request.getSenderCode(), request.getRecipientCode(), getEntity(request.getAction()), request.getPayload().getOrDefault(Constants.PAYLOAD, ""), reqFhirObj, respFhirObj, PENDING, JSONUtils.serialize(info), System.currentTimeMillis(), System.currentTimeMillis(), request.getCorrelationId(), "", PENDING, request.getWorkflowId(), "1234", "1234", PENDING, "", "{}", "", "", "");
         } else {
             info.put("medical", Collections.singletonMap("status", PENDING));
             info.put("financial", Collections.singletonMap("status", PENDING));
