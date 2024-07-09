989eb5273a67886e96683607fb1825258337239d
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2024-07-09 20:00:43 +0530

    changed websocket pattern

--- src/main/java/org/swasth/hcx/config/WebSocketConfig.java
+++ src/main/java/org/swasth/hcx/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:5173").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
