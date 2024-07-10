0289be547455109a7c25737edb10005d72b14908
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2024-07-10 09:55:30 +0530

    Updated websocket origins url

--- src/main/java/org/swasth/hcx/config/WebSocketConfig.java
+++ src/main/java/org/swasth/hcx/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package org.swasth.hcx.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,15 +11,24 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${hcx_application.url}")
+    private String urlPattern;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        System.out.println("Allowed Origins for WebSocket " + getAllowedOrigins().toString());
+        registry.addEndpoint("/ws").setAllowedOriginPatterns(getAllowedOrigins()).withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
+    }
+
+    private String[] getAllowedOrigins() {
+        // Define the base URL and add a wildcard for any subpath
+        return new String[] {urlPattern + "*"};
     }
 }
 
